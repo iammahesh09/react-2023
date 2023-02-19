7481a925c833ace55af49a4d400225244b607378
iammahesh09 <iammahesh.nlr@gmail.com>
2023-02-19 21:39:49 +0530

    React

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import RouterInDepth from './RouterInDepth';
 function App () {
   return (
     <div className="container">
-      {/* <h1 className='text-center text-primary'>Hello 2022</h1> */}
+      {/* <h1 className='text-center text-primary'>Hello 2023</h1> */}
       <RouterInDepth />
     </div>
   );
